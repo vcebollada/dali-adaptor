dfa69a73527384a2d0d3463ae897b82fad6fd29a
Anton Obzhirov <a.obzhirov@samsung.com>
2019-10-30 12:00:04 +0000

    Fix cairo renderer compilation issues.
    Change-Id: Ic4996c53c4e179390f1ebc02d717934de9c55333

--- dali/internal/text/text-abstraction/text-renderer-impl.cpp
+++ dali/internal/text/text-abstraction/text-renderer-impl.cpp
@@ -66,11 +66,7 @@ TextAbstraction::TextRenderer TextRenderer::Get()
 
 Devel::PixelBuffer TextRenderer::Render(const TextAbstraction::TextRenderer::Parameters& parameters)
 {
-#ifdef ANDROID
-  return Devel::PixelBuffer();
-#else
   return RenderTextCairo(parameters);
-#endif
 }
 
 } // namespace Internal
